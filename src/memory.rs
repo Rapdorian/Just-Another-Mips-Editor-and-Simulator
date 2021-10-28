068737cb72d6620442ac82913c50c4a0843fbd19
James Pruitt <jamescpruitt@gmail.com>
2021-10-28 17:24:16 -0400

    Memory struct now has realistic addresses
    The memory struct has been rewritten to be byte-addressed. I have also
    added a number of methods for reading/writing to memory aligned words
    and halfwords.

--- src/memory.rs
+++ src/memory.rs
@@ -1,10 +1,15 @@
+use thiserror::Error;
+
+/// Memory error struct
+#[derive(Error, Debug)]
+pub enum MemoryError {
+    #[error("Unaligned memory access on {addr}, expected address to be aligned to {align} byte")]
+    UnalignedAccess { addr: u32, align: u32 },
+}
+
 /// Handles memory
-///
-/// For now this will just be a thin wrapper around an array
-///
-/// TODO: Better memory address (currently they make no sense)
 pub struct Memory {
-    data: Vec<u32>,
+    data: Vec<u8>,
 }
 
 impl Memory {
@@ -15,11 +20,74 @@ impl Memory {
         }
     }
 
-    pub fn read(&self, address: u32) -> u32 {
-        self.data[address as usize]
+    /// Read a byte from memory
+    pub fn read(&self, address: u32) -> Result<u8, MemoryError> {
+        Ok(self.data[address as usize])
     }
 
-    pub fn write(&mut self, address: u32, data: u32) {
+    /// Read an aligned halfword from memory
+    pub fn read_half(&self, address: u32) -> Result<u16, MemoryError> {
+        // check that the address is halfword aligned
+        if address % 2 != 0 {
+            return Err(MemoryError::UnalignedAccess {
+                addr: address,
+                align: 2,
+            });
+        }
+        let lsb = self.read(address)? as u16;
+        let msb = self.read(address + 1)? as u16;
+        Ok((msb << 8) + lsb)
+    }
+
+    /// Read an aligned word from memory
+    pub fn read_word(&self, address: u32) -> Result<u32, MemoryError> {
+        // check that the address is word aligned
+        if address % 4 != 0 {
+            return Err(MemoryError::UnalignedAccess {
+                addr: address,
+                align: 4,
+            });
+        }
+        let lsh = self.read_half(address)? as u32;
+        let msh = self.read_half(address + 2)? as u32;
+        Ok((msh << 16) + lsh)
+    }
+
+    /// Write a byte to memory
+    pub fn write(&mut self, address: u32, data: u8) -> Result<(), MemoryError> {
         self.data[address as usize] = data;
+        Ok(())
+    }
+
+    /// Write an aligned halfword to memory
+    pub fn write_half(&mut self, address: u32, data: u16) -> Result<(), MemoryError> {
+        if address % 2 != 0 {
+            return Err(MemoryError::UnalignedAccess {
+                addr: address,
+                align: 2,
+            });
+        }
+
+        let bytes = data.to_le_bytes();
+        self.data[address as usize] = bytes[0];
+        self.data[address as usize + 1] = bytes[1];
+        Ok(())
+    }
+
+    /// Write an aligned word to memory
+    pub fn write_word(&mut self, address: u32, data: u32) -> Result<(), MemoryError> {
+        if address % 4 != 0 {
+            return Err(MemoryError::UnalignedAccess {
+                addr: address,
+                align: 4,
+            });
+        }
+
+        let bytes = data.to_le_bytes();
+        self.data[address as usize] = bytes[0];
+        self.data[address as usize + 1] = bytes[1];
+        self.data[address as usize + 2] = bytes[2];
+        self.data[address as usize + 3] = bytes[3];
+        Ok(())
     }
 }
