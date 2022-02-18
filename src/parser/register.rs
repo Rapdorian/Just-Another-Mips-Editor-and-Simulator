b76355dc3f2d0be30e309ef273fc3a0b1139232e
James Pruitt <jamescpruitt@gmail.com>
2022-02-18 00:07:26 -0500

    Added J and BEQ parsing and J simulation

--- src/parser/register.rs
+++ src/parser/register.rs
@@ -4,20 +4,21 @@ use nom::{
     bytes::complete::{tag, take_till},
     character::complete::multispace0,
     combinator::{map_res, opt},
+    error::VerboseError,
     sequence::preceded,
     IResult,
 };
 
 use crate::Register;
 
-pub fn register_name(input: &str) -> IResult<&str, Register> {
+pub fn register_name(input: &str) -> IResult<&str, Register, VerboseError<&str>> {
     map_res(
         take_till(|c: char| c.is_whitespace() || c == ',' || c == ')' || c == '#'),
         |name: &str| Register::try_from(name),
     )(input)
 }
 
-pub fn register(input: &str) -> IResult<&str, Register> {
+pub fn register(input: &str) -> IResult<&str, Register, VerboseError<&str>> {
     let (input, _) = multispace0(input)?;
     let (input, _) = tag("$")(input)?;
     let (input, reg) = register_name(input)?;
