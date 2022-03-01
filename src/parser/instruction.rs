58d66cb0455f9d1e586a0c341dcd21fd836becc7
James Pruitt <jamescpruitt@gmail.com>
2022-02-28 20:25:10 -0500

    Refactor parser for cleaner structure (#10)
    parser now has an easier to follow structure which allows better error
    messages and more strict syntax checking

--- src/parser/instruction.rs
+++ src/parser/instruction.rs
@@ -3,7 +3,7 @@ use nom::{
     bytes::complete::{tag, take_till},
     character::complete::{alphanumeric1, multispace0},
     combinator::{map, map_res, opt},
-    error::VerboseError,
+    error::{context, VerboseError},
     multi::many1,
     sequence::{delimited, preceded, tuple},
     IResult,
@@ -16,7 +16,6 @@ use crate::{
 
 use super::{
     model::{Imm, Instruction, Line, Symbol},
-    opcode::Style,
     ParseError,
 };
 
@@ -40,21 +39,20 @@ fn symbol(input: &str) -> IResult<&str, Symbol, VerboseError<&str>> {
     )(input)
 }
 
-fn j_type(input: &str) -> IResult<&str, Vec<Instruction>, VerboseError<&str>> {
-    // j format instructions should look like
-    // <OP> <ADDRESS>
-    let (input, (op, style)) = parser::opcode(input)?;
-    let (input, addr) = symbol(input)?;
+type ParserOutput<'a> = IResult<&'a str, Vec<Instruction>, VerboseError<&'a str>>;
+
+/// Parse jump instructions
+/// <OP> <label>
+pub fn j_type(input: &str, op: Opcode) -> ParserOutput {
+    let (input, addr) = context("Expected label", symbol)(input)?;
     Ok((input, vec![Instruction::J { op, addr }]))
 }
 
-fn r_type(input: &str) -> IResult<&str, Vec<Instruction>, VerboseError<&str>> {
-    // all r format instruction should look like
-    // op rd, rs, rt
-    let (input, (op, style)) = parser::opcode(input)?;
-    let (input, (rd, rs, rt)) =
-        tuple((parser::register, parser::register, parser::register))(input)?;
-    // TODO: support shift instructions
+/// Parse JR instruction
+pub fn jr_type(input: &str, op: Opcode) -> ParserOutput {
+    let (input, rs) = context("Expected register", parser::register)(input)?;
+    let rt = ZERO;
+    let rd = ZERO;
     Ok((
         input,
         vec![Instruction::R {
@@ -67,74 +65,81 @@ fn r_type(input: &str) -> IResult<&str, Vec<Instruction>, VerboseError<&str>> {
     ))
 }
 
-fn i_type(input: &str) -> IResult<&str, Vec<Instruction>, VerboseError<&str>> {
-    let (input, (op, style)) = parser::opcode(input)?;
-    let (input, _) = opt(tag(","))(input)?;
-    let (input, rt) = parser::register(input)?;
-    let (input, _) = multispace0(input)?;
+/// Parses simple R-type instructions using the format
+/// `<OP> <rd>, <rs>, <rt>`
+pub fn r_type(input: &str, op: Opcode) -> ParserOutput {
+    let (input, (rd, rs, rt)) = context(
+        "Expected format <op> rd, rs, rt",
+        tuple((parser::register, parser::register, parser::register)),
+    )(input)?;
+    Ok((
+        input,
+        vec![Instruction::R {
+            op,
+            rd,
+            rs,
+            rt,
+            shamt: 0,
+        }],
+    ))
+}
 
-    // options for second arg
-    // add style: rs, imm
-    // mem style: imm(rs)
-    // TODO: I'm not going to do style checking for now
-    // technically something like `lw $t0, $t2, 4` or `addi $t0, 1($t1)`
-    // will be accepted
+/// Parses simple immediate mode instructions using the format
+/// `<OP> <rt> <rs> <imm>`
+pub fn i_type(input: &str, op: Opcode) -> ParserOutput {
+    let (input, rt) = context("Expected target register", parser::register)(input)?;
+    let (input, rs) = context("Expected source register", parser::register)(input)?;
+    let (input, imm) = context("Expected immediate value", immediate)(input)?;
+    Ok((input, vec![Instruction::I { op, rt, rs, imm }]))
+}
 
-    let add_style = tuple((parser::register, preceded(multispace0, immediate)))(input);
+/// Parses load and store instructions
+/// `<OP> <rt> <imm>(<rs>)
+pub fn load_type(input: &str, op: Opcode) -> ParserOutput {
+    let (input, rt) = context("Expected target register", parser::register)(input)?;
+    let (input, imm) = context("Expected offset value", immediate)(input)?;
+    let (input, rs) = context(
+        "Expected source value",
+        delimited(tag("("), parser::register, tag(")")),
+    )(input)?;
+    Ok((input, vec![Instruction::I { op, rt, rs, imm }]))
+}
 
-    let rs;
-    let imm;
-    let fin_input;
+/// Parses branch instructions
+/// `<OP> <rt> <rs> <label>`
+pub fn branch_type(input: &str, op: Opcode) -> ParserOutput {
+    let (input, rt) = context("Expected first register", parser::register)(input)?;
+    let (input, rs) = context("Expected second register", parser::register)(input)?;
+    let (input, mut imm) = context("Expected label", immediate)(input)?;
 
-    if let Ok(add_style) = add_style {
-        // extract data
-        let (input, (reg, mut num)): (&str, (Register, Imm)) = add_style;
-        if let Style::PcRelative = style {
-            if let Imm::Label(label) = num {
-                num = Imm::PcRelative(label);
-            }
-        }
-        rs = reg;
-        imm = num;
-        fin_input = input;
-    } else {
-        // try other style
-        let (input, (num, reg)): (&str, (i64, Register)) =
-            tuple((parser::int, delimited(tag("("), parser::register, tag(")"))))(input)?;
-        rs = reg;
-        imm = Imm::Value(num);
-        fin_input = input;
+    // if we got a label make it pc relative
+    if let Imm::Label(label) = imm {
+        imm = Imm::PcRelative(label);
     }
-
-    Ok((fin_input, vec![Instruction::I { op, rt, rs, imm }]))
+    Ok((input, vec![Instruction::I { op, rt, rs, imm }]))
 }
 
-/// Currently supported pseudoinstructions:
-/// - `li` : Load immediate
-/// - `la` : Load address
-/// - `move` : Move address
-fn move_ins(input: &str) -> IResult<&str, Vec<Instruction>, VerboseError<&str>> {
-    map(
-        preceded(tag("move"), tuple((parser::register, parser::register))),
-        |regs| {
-            vec![Instruction::R {
-                op: Opcode::Funct(0x20), // add
-                rd: regs.0,
-                rs: regs.1,
-                rt: ZERO,
-                shamt: 0,
-            }]
-        },
-    )(input)
+/// Parses a move pseudoinstruction
+pub fn move_ins(input: &str) -> ParserOutput {
+    let (input, rd) = context("Expected destination register", parser::register)(input)?;
+    let (input, rs) = context("Expected source register", parser::register)(input)?;
+    Ok((
+        input,
+        vec![Instruction::R {
+            op: Opcode::Funct(0x20), // add
+            rd,
+            rs,
+            rt: ZERO,
+            shamt: 0,
+        }],
+    ))
 }
 
-fn li_ins(input: &str) -> IResult<&str, Vec<Instruction>, VerboseError<&str>> {
+/// Parses li and la instructions
+pub fn li_ins(input: &str) -> ParserOutput {
     // li and la are the same
     map(
-        preceded(
-            alt((tag("li"), tag("la"))),
-            tuple((parser::register, immediate)),
-        ),
+        tuple((parser::register, immediate)),
         |(reg, imm)| match imm {
             Imm::Label(ref name) => vec![
                 // TODO: Make loads >16bits work
@@ -193,8 +198,7 @@ fn li_ins(input: &str) -> IResult<&str, Vec<Instruction>, VerboseError<&str>> {
     )(input)
 }
 
-pub fn syscall(input: &str) -> IResult<&str, Vec<Instruction>, VerboseError<&str>> {
-    let (input, _) = delimited(multispace0, tag("syscall"), multispace0)(input)?;
+pub fn syscall(input: &str) -> ParserOutput {
     Ok((
         input,
         vec![Instruction::R {
@@ -207,59 +211,50 @@ pub fn syscall(input: &str) -> IResult<&str, Vec<Instruction>, VerboseError<&str
     ))
 }
 
-pub fn word_lit(input: &str) -> IResult<&str, Vec<Instruction>, VerboseError<&str>> {
-    preceded(
-        tag(".word"),
-        many1(map(
-            delimited(multispace0, parser::int, opt(tag(","))),
-            |i: i64| Instruction::Literal { data: i as u32 },
-        )),
-    )(input)
+pub fn word_lit(input: &str) -> ParserOutput {
+    many1(map(
+        delimited(multispace0, parser::int, opt(tag(","))),
+        |i: i64| Instruction::Literal { data: i as u32 },
+    ))(input)
 }
 
-pub fn nop(input: &str) -> IResult<&str, Vec<Instruction>, VerboseError<&str>> {
-    let (input, _) = delimited(multispace0, tag("nop"), multispace0)(input)?;
+pub fn nop(input: &str) -> ParserOutput {
     Ok((input, vec![Instruction::Literal { data: 0 }]))
 }
 
-pub fn ascii_lit(input: &str) -> IResult<&str, Vec<Instruction>, VerboseError<&str>> {
-    preceded(
-        tag(".ascii"),
-        delimited(
-            multispace0,
-            map(
-                delimited(tag("\""), take_till(|c: char| c == '"'), tag("\"")),
-                |s: &str| {
-                    // escape stuff
-                    let s = s.replace("\\n", "\n");
-                    let s = s.replace("\\0", "\0");
-                    // convert to bytes
-                    let bytes = s.as_bytes();
-                    let mut words = vec![];
-                    while words.len() * 4 < bytes.len() {
-                        let i = words.len() * 4;
-                        words.push(Instruction::Literal {
-                            data: u32::from_ne_bytes([
-                                *bytes.get(i).unwrap_or(&0),
-                                *bytes.get(i + 1).unwrap_or(&0),
-                                *bytes.get(i + 2).unwrap_or(&0),
-                                *bytes.get(i + 3).unwrap_or(&0),
-                            ]),
-                        });
-                    }
-                    words
-                },
-            ),
-            opt(tag(",")),
+pub fn ascii_lit(input: &str) -> ParserOutput {
+    delimited(
+        multispace0,
+        map(
+            delimited(tag("\""), take_till(|c: char| c == '"'), tag("\"")),
+            |s: &str| {
+                // escape stuff
+                let s = s.replace("\\n", "\n");
+                let s = s.replace("\\0", "\0");
+                // convert to bytes
+                let bytes = s.as_bytes();
+                let mut words = vec![];
+                while words.len() * 4 < bytes.len() {
+                    let i = words.len() * 4;
+                    words.push(Instruction::Literal {
+                        data: u32::from_ne_bytes([
+                            *bytes.get(i).unwrap_or(&0),
+                            *bytes.get(i + 1).unwrap_or(&0),
+                            *bytes.get(i + 2).unwrap_or(&0),
+                            *bytes.get(i + 3).unwrap_or(&0),
+                        ]),
+                    });
+                }
+                words
+            },
         ),
+        opt(tag(",")),
     )(input)
 }
 
 pub fn instruction(input: &str) -> IResult<&str, Line, VerboseError<&str>> {
-    map(
-        alt((
-            r_type, i_type, j_type, nop, move_ins, li_ins, syscall, word_lit, ascii_lit,
-        )),
-        |x| Line::Instruction(x),
-    )(input)
+    // grab the opcode
+    let (input, parser) = preceded(multispace0, parser::opcode)(input)?;
+    let parser = move |input| parser.parse(input);
+    map(parser, |x| Line::Instruction(x))(input)
 }
